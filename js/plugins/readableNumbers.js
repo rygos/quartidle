871fd4326b3532a5080aa8f5cec65d2522fc78ca
rygos <rygos@icloud.com>
2021-01-10 20:22:40 +0100

    1.0.0
    Release Version

---
+++ js/plugins/readableNumbers.js
@@ -0,0 +1,58 @@
+/*:
+*
+* @plugindesc SimpleReadableNumbers
+*
+*@author ryg
+*
+*@help
+*
+*/
+
+(function () {
+
+	SelectRandomName = function(file) {
+		var namesArray = ReadNamesFromTxt(file);
+		var random = Math.floor(Math.random() * (namesArray.length));
+		var returnedName = namesArray[random];
+		returnedName = returnedName.charAt(0).toUpperCase() + returnedName.slice(1);
+		return returnedName;
+	}
+
+abbrNum = function(number, decPlaces) {
+    // 2 decimal places => 100, 3 => 1000, etc
+    decPlaces = Math.pow(10,decPlaces);
+
+    // Enumerate number abbreviations
+    var abbrev = [ " Tsd", " Mio", "Mil", "Tril" ];
+
+    // Go through the array backwards, so we do the largest first
+    for (var i=abbrev.length-1; i>=0; i--) {
+
+        // Convert array index to "1000", "1000000", etc
+        var size = Math.pow(10,(i+1)*3);
+
+        // If the number is bigger or equal do the abbreviation
+        if(size <= number) {
+             // Here, we multiply by decPlaces, round, and then divide by decPlaces.
+             // This gives us nice rounding to a particular decimal place.
+             number = Math.round(number*decPlaces/size)/decPlaces;
+
+             // Handle special case where we round up to the next abbreviation
+             if((number == 1000) && (i < abbrev.length - 1)) {
+                 number = 1;
+                 i++;
+             }
+
+             // Add the letter for the abbreviation
+             number += abbrev[i];
+
+             // We are done... stop
+             break;
+        }
+    }
+
+    return number;
+}
+
+
+})();
